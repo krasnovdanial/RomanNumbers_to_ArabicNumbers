64db73ac96d20bd88d4eb6491da32f81f558d0d3
Daniel Krasnov <58078456+krasnovdanial@users.noreply.github.com>
2021-11-25 21:00:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+public class Roman
+{
+
+	public static string NumberToRoman(int number)
+	{
+		if (number < 0 || number > 3999)
+        {
+			Console.WriteLine("Oops, that's number couldn't convert to a Roman number");
+		}
+			
+
+		if (number == 0)
+        {
+			return "0 doesn't exist in Roman numbers";
+		}
+			
+		int[] values = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+		string[] numerals = new string[]
+		{ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+
+		StringBuilder result = new StringBuilder();
+
+
+		for (int i = 0; i < 13; i++)
+		{
+			while (number >= values[i])
+			{
+				number -= values[i];
+				result.Append(numerals[i]);
+			}
+		}
+
+		return result.ToString();
+	}
+
+
+	public static void Main()
+	{
+		Console.WriteLine(NumberToRoman(50));
+	}
+}
